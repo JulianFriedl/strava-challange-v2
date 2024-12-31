0766e9ca8e8b8703fb90a1e820a55bf8c67182fb
ge48lib <lukas.weingand@tum.de>
2024-12-31 02:01:49 +0100

    add auth to leaderboard

--- backend/src/api/leaderboard.py
+++ backend/src/api/leaderboard.py
@@ -10,10 +10,15 @@ logger = logging.getLogger(__name__)
 leaderboard_blueprint = Blueprint('leaderboard', __name__)
 
 @leaderboard_blueprint.route('/', methods=['GET'])
+@leaderboard_blueprint.route('', methods=['GET'])
 def leaderboard():
     """
     Generate and return the total leaderboard as well as all sub-leaderboards.
     """
+    if not session.get("user_id"):
+        logger.info("Not logged in.")
+        return jsonify({"error": "unauthenticated"}), 401
+
     logger.info("Leaderboard request received.")
     try:
         athlete_activities = get_all_athlete_activities()
